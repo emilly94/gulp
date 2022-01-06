cccf1f82eb2c4f9b5ffb16dcfd1f88d340575a5b
emilly94 <emillysporto.94@gmail.com>
2022-01-06 15:47:48 -0300

    refatorando arquivos para o gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const gulp = require("gulp");
 
 function tarefasCss(){
   return gulp.src('./vendor/**/*.css')
-  .pipe(concat('libs.css'))
+  /*.pipe(concat('libs.css'))
   .pipe(cssmin())
-  .pipe(rename({ suffix: '.min'}))
+  .pipe(rename({ suffix: '.min'}))*/
   .pipe(gulp.dest('.dist/css'))
 }
 
